4015892fafff3db7dcd4b083ba0152d2dae70e4a
natalia-1707 <nataliatsepoukhowa@gmail.com>
2026-06-06 22:48:56 +0300

    feat: modals using React Portals and ESC key to close

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,6 @@
 import "./modal.css"
+import { useEffect } from "react";
+import { createPortal } from "react-dom";
 
 type ModalProps = {
   title: string;
@@ -6,15 +8,31 @@ type ModalProps = {
   onClose: () => void;
 };
 
+const modalRoot = document.getElementById("modal-root")!;
 
 export default function Modal({title, children, onClose}: ModalProps) {
-    return (
+    useEffect(() => {
+      const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+      };
+
+      window.addEventListener("keydown", handleKeyDown);
+
+      return () => {
+        window.removeEventListener("keydown", handleKeyDown);
+      };
+    }, [onClose]);
+
+    return createPortal(
       <div className="modal-overlay" onClick={onClose}>
         <div className="modal-wrapper" onClick={(e) => e.stopPropagation()}>
             <span className="material-symbols-outlined" onClick={onClose}>close</span>
             <h3>{title}</h3>
             <div className="forms-field">{children}</div>
         </div>
-      </div>
+      </div>,
+      modalRoot
     )
 }
