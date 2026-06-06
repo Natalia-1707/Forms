eab8c850af82b259cde883fc6bc2022a95b39e66
natalia-1707 <nataliatsepoukhowa@gmail.com>
2026-06-06 20:16:09 +0300

    feat: make initial page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-/* import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png' */
 import './App.css'
+import Header from './components/Header';
+import Main from './components/Main'
 
 function App() {
-  return <h1>Forms App</h1>;
+  return (
+    <div className="app-wrapper">
+      <Header />
+      <Main />
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/Main.tsx
@@ -0,0 +1,15 @@
+import "./main.css"
+
+export default function Main() {
+    return (
+        <div className="main-wrapper">
+            <div className="buttons-div">
+                <button>Uncontrolled Form</button>
+                <button>Open RHF Form</button>
+            </div>
+            <div className="Submissions">
+                <h2>Submissions</h2>
+            </div>
+        </div>
+    )
+}
